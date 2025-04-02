d63ccf8f49b3fc937e54780f37a9e0856d489d40
Jozef Hrdy <jozefhrdy2002@gmail.com>
2025-04-02 21:17:28 +0200

    typo fix

--- db_test.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("database.db")
-cursor = conn.cursor()
-cursor.execute("""select * from users;""")
-users = cursor.fetchall()
-
-print(users)
--- send_email.py
+++ send_email.py
@@ -5,8 +5,9 @@ with open("classified.json", "r") as file:
     data = json.load(file)
 
 email = data.get("email")
-password = data.get("password")
+password = data.get("pass")
 
 def send_email_to(reciever,message):
+    print('sending mail')
     yag = yagmail.SMTP(user=email, password=password)
     yag.send(to=reciever, subject="Nakupny zoznam", contents=message)
